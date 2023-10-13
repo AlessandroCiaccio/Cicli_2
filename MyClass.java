f02dc5a3f2b07d20b1e631fa4d655eed490039d8
Alessandro <sandroscro@libero.it>
2023-10-13 15:48:28 +0200

    Added Main

---
+++ MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int x=10;
+      for (int i=0; i<=x;i++){
+          if(i==5){
+              i++;
+          }
+          System.out.println(i);
+      }
+    }
+}
