f90e81ffd0683c66d96bfd0be185cefa853380fe
Alessandro <sandroscro@libero.it>
2023-10-17 12:46:16 +0200

    Metodo aggiunto

--- MyClass.java
+++ MyClass.java
@@ -1,11 +1,14 @@
 public class MyClass {
     public static void main(String args[]) {
-      int x=10;
-      for (int i=0; i<=x;i++){
-          if(i==5){
-              i++;
-          }
-          System.out.println(i);
-      }
+      int x=17;
+      stampaNumeri(x);
+    }
+    public static void stampaNumeri(int x){
+        for (int i=0; i<=x;i++){
+            if(i==5){
+                i++;
+            }
+            System.out.print(i+"  ");
+        }
     }
 }
